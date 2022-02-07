5bfdd0aafc3357a91d79ee5790353f48f2cb4651
unknown <maniplek81@gmail.com>
2022-02-07 16:04:45 +0200

    added hooks and styles

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,41 @@
+import { useState } from 'react'
+import { FaTrashAlt} from 'react-icons/fa'
 function Content() {
-    const handleNameChange = () => {
-        const names = ['Bob', 'kevin' , 'Dave']
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
-    }
+    const [items , setItems] = useState([
+        {
+            id:1,
+            checked: false,
+            item: "One half pound bag of Cocoa Covered Almonds"
+        },
+        {
+            id:2,
+            checked: false,
+            item: "item 2 "
+        },
+        {
+            id:3,
+            checked: false,
+            item: "item 3 "
+        }
+    ]);
+   
   return (
   <main>
-      <p> Hello { handleNameChange() }! </p>
+       <ul>
+            {items.map(item => (
+                <li className='item' key={item.id}>
+                    <input
+                    type='checkbox'
+                    checked={item.checked}
+                    />
+                    <label>{item.item}</label>
+                   <FaTrashAlt 
+                      role="button"
+                      tabIndex="0"
+                    />
+                </li>
+            ))}
+        </ul>     
   </main>);
 }
 
