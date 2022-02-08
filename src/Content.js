3ac926b10177230865915fa0184f520a52256a10
unknown <maniplek81@gmail.com>
2022-02-08 11:47:47 +0200

    added delete function in content component

--- src/Content.js
+++ src/Content.js
@@ -19,23 +19,45 @@ function Content() {
         }
     ]);
    
+    const handleCheck = ( id ) =>{
+        const listItems = items.map((item) => item.id === id ? 
+        {...item,checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+
+    }
+    const handleDelete = ((id)=>{
+        const listItems =  items.filter((item)=> item.id !== id);
+        setItems(listItems)
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    })
+
   return (
   <main>
+      {items.length ? (
        <ul>
             {items.map(item => (
                 <li className='item' key={item.id}>
                     <input
                     type='checkbox'
+                    onChange={() => handleCheck(item.id)}
                     checked={item.checked}
                     />
-                    <label>{item.item}</label>
-                   <FaTrashAlt 
+                    <label
+                        style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                        onDoubleClick={() => handleCheck(item.id)}
+                    >{item.item}</label>
+                   <FaTrashAlt
+                      onClick={() => handleDelete(item.id)} 
                       role="button"
                       tabIndex="0"
                     />
                 </li>
             ))}
-        </ul>     
+        </ul>   
+      ) :(
+          <p style={{marginTop: '2rem'}}>Your List Is Empty.</p>
+      ) }
   </main>);
 }
 
